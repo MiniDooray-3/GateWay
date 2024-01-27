631e0ab9f6769650ef7d8f2fa4673eaed8e98b8d
Huni0819 <orutaku15@gmail.com>
2024-01-27 17:59:55 +0900

    Feat: LoginInterceptor

--- src/main/java/com/nhnacademy/edu/minidooray/interceptor/LoginInterceptor.java
+++ src/main/java/com/nhnacademy/edu/minidooray/interceptor/LoginInterceptor.java
@@ -1,8 +1,10 @@
 package com.nhnacademy.edu.minidooray.interceptor;
 
 
+import java.util.Objects;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 public class LoginInterceptor implements HandlerInterceptor {
@@ -10,7 +12,13 @@ public class LoginInterceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
             throws Exception {
-        // TODO: Login Interceptor 작성
-        return HandlerInterceptor.super.preHandle(request, response, handler);
+        HttpSession session = request.getSession(false);
+
+        if (Objects.isNull(session) || Objects.isNull(session.getAttribute("LOGIN_ID"))) {
+            response.sendRedirect("/login");
+            return false;
+        }
+
+        return true;
     }
 }
